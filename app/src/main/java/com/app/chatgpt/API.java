861958ada295a7a213033080fab524c4c2e54fa8
Ajwyunsx <107187521+Ajwyunsx@users.noreply.github.com>
2023-09-02 22:09:26 +0800

    Update API.java

--- app/src/main/java/com/app/chatgpt/API.java
+++ app/src/main/java/com/app/chatgpt/API.java
@@ -1,6 +1,6 @@
 package com.app.chatgpt;
 
 public class API {
-    public static String API_URL = "https://api.chatanywhere.com.cn/v1/completions";
+    public static String API_URL = "https://api.chatanywhere.cn/v1/completions";
     public static String API = "sk-Nn4MSf4TFdBSXpOQ84Tfst4JRjt4jltzXUjKj9ZB2cnHVZze";
 }
